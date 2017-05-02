65c8b2e6efbe4218947679103c64c63fccad95e8
kiritow <1362050620@qq.com>
2017-05-02 12:59:57 +0800

    Add Minimum Event Support

--- MiniEngine_Event.cpp
+++ MiniEngine_Event.cpp
@@ -1,1 +1,21 @@
 #include "MiniEngine_Event.h"
+
+int PollEvent(Event& refEvent)
+{
+    return SDL_PollEvent(&refEvent);
+}
+
+int WaitEvent(Event& refEvent)
+{
+    return SDL_WaitEvent(&refEvent);
+}
+
+int WaitEventTimeout(Event& refEvent,int ms)
+{
+    return SDL_WaitEventTimeout(&refEvent,ms);
+}
+
+int PushEvent(const Event& refEvent)
+{
+    return SDL_PushEvent(const_cast<Event*>(&refEvent));
+}
--- MiniEngine_Event.h
+++ MiniEngine_Event.h
@@ -1,2 +1,9 @@
 #pragma once
 #include "MiniEngine.h"
+
+typedef SDL_Event Event;
+
+int PollEvent(Event& refEvent);
+int WaitEvent(Event& refEvent);
+int WaitEventTimeout(Event& refEvent,int ms);
+int PushEvent(const Event& refEvent);
