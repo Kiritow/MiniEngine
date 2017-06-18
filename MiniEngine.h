c86b52ef19b673602c42a78a651a37ca8fca8757
kiritow <1362050620@qq.com>
2017-06-18 17:43:23 +0800

    Move cursor out

--- MiniEngine.h
+++ MiniEngine.h
@@ -62,29 +62,6 @@ namespace MiniEngine
         No, Hand
     };
 
-	class Cursor
-	{
-    public:
-        Cursor()=default;
-        Cursor(SystemCursorType);
-        Cursor(Surface surf,Point hotspot={0,0});
-
-        static Cursor GetActiveCursor();
-        static Cursor GetDefaultCursor();
-
-        static void setShow(bool);
-        static bool isShow();
-
-        void activate();
-
-        void release();
-    private:
-        std::shared_ptr<SDL_Cursor> _cur;
-        void _set(SDL_Cursor*);
-        void _set_no_delete(SDL_Cursor*);
-        SDL_Cursor* _get();
-        void _clear();
-	};
 
 	enum class MessageBoxType { Error, Warning, Information };
 
---
+++ SDLWrapper/Cursor.cpp
@@ -0,0 +1,80 @@
+#include "Cursor.h"
+#include "begin_code.h"
+//private
+void Cursor::_set(SDL_Cursor* p)
+{
+    _cur.reset(p,SDL_FreeCursor);
+}
+
+//private
+void Cursor::_set_no_delete(SDL_Cursor* p)
+{
+    _cur.reset(p,[](SDL_Cursor* p) {});
+}
+
+//private
+SDL_Cursor* Cursor::_get()
+{
+    return _cur.get();
+}
+
+//private
+void Cursor::_clear()
+{
+    _cur.reset();
+}
+
+Cursor::Cursor(Surface surf,Point hotspot)
+{
+    Cursor ns;
+    SDL_Cursor* cursor=SDL_CreateColorCursor(surf._get(),hotspot.x,hotspot.y);
+    ns._set(cursor);
+}
+
+Cursor::Cursor(SystemCursorType type)
+{
+    Cursor ns;
+    ns._set(SDL_CreateSystemCursor(_internal::getSDLSystemCursorFromSystemCursorType(type)));
+}
+
+//static
+Cursor Cursor::GetActiveCursor()
+{
+    Cursor ns;
+    ns._set_no_delete(SDL_GetCursor());
+    return ns;
+}
+
+//static
+Cursor Cursor::GetDefaultCursor()
+{
+    Cursor ns;
+    ns._set_no_delete(SDL_GetDefaultCursor());
+    return ns;
+}
+
+//static
+bool Cursor::isShow()
+{
+    return (SDL_ShowCursor(SDL_QUERY)==SDL_ENABLE);
+}
+
+//static
+void Cursor::setShow(bool Settings)
+{
+    SDL_ShowCursor(Settings?SDL_ENABLE:SDL_DISABLE);
+}
+
+void Cursor::release()
+{
+    _clear();
+}
+
+void Cursor::activate()
+{
+    if(_get()!=nullptr)
+    {
+        SDL_SetCursor(_get());
+    }
+}
+#include "end_code.h"
